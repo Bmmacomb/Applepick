4d5444af498c1827399f79a913510c358cd003d9
Brendan Mulhern <bmmacomb@sbcglobal.net>
2015-01-15 12:26:21 -0500

    a program for class

---
+++ Assets/ApplePicker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplePicker : MonoBehaviour {
+	public GameObject basketPrefab;
+	public int num_baskets = 3;
+	public float basketBotY = -14f;
+	public float basketSpaY = 2f;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < num_baskets; i++) {
+			GameObject tBasketGo = Instantiate(basketPrefab) as GameObject;
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBotY + (basketSpaY * i);
+			tBasketGo.transform.position = pos;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/AppleTree.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+	//Prefab for inst apples
+	public GameObject applePrefab;
+
+	//speed at which tree moves
+	public float speed = 1f;
+
+	//dist when tree turns around
+	public float Left_Right_edge = 10f;
+
+	// chance tree will change direction
+	public float chanceToChangeDir = 0.02f;
+
+	//rate at which apples will be instanciated
+	public float secBtwnAppleDrops = 1f;
+
+	// Use this for initialization
+	void Start() { InvokeRepeating ("DropApple", 2f, secBtwnAppleDrops);
+	
+	}
+
+	void DropApple(){
+		GameObject apple = Instantiate(applePrefab) as GameObject;
+		apple.transform.position = transform.position;
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		// basic movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		// cng diretion
+		if (pos.x < -Left_Right_edge) {
+			speed = Mathf.Abs(speed);//move right
+		}
+		else if (pos.x > Left_Right_edge){
+			speed = -Mathf.Abs(speed);//move left
+
+		}
+	
+	
+}
+	void FixedUpdate(){
+		// change dir randomly
+		if (Random.value < chanceToChangeDir){
+			speed *= -1;
+		}
+
+	}
+}
